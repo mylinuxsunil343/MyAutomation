264a5cd042e8cf4c7974204d5a86ed9819b8a3f9
Sunil Marella <mylinux.sunil343@gmail.com>
2022-12-17 16:22:33 +0100

    Python

---
+++ Python/Program/01-Welcome.py
@@ -0,0 +1,8 @@
+print ("Hello, World!");
+
+# This is comment section
+
+'''
+This is multiline comment
+Line 2
+'''
---
+++ Python/Program/02-Variables.py
@@ -0,0 +1,5 @@
+counter = 100          # Creates an integer variable
+miles   = 1000.0       # Creates a floating point variable
+name    = "Sunil Marella"   # Creates a string variable
+
+print('Print value : ',counter)
